a087b637243e7e244210e5a06133cf7b61ada933
MarcDufresne <marc.andre.dufresne@gmail.com>
2016-01-23 14:38:39 -0500

    SteamPrice crawler, first version

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-pushbullet-crawler
+SteamPrice Crawler
 ==================
 
-A web crawler that detects a regex on a page and sends a notification
+A web crawler that watches prices for games on SteamPrice.
+
+Usage:
+
+    ./scrapper.py -r <region> -a <game_id[,game_id,...]> -k <pushbullet_api_key>
+    
+    region: ca, us, uk, etc. (found in the URL on SteamPrice)
+    game_id: comma separated list of numbers (e.g. 238460 or 238460,252950; found in the URL on SteamPrice)
+    pushbullet_api_key: your API key for PushBullet
+    
+This script is intended to be used in a cron job
+
+    0 */6 * * * /path/to/scrapper.py -r <region> -a <game_id[,game_id,...]> -k <pushbullet_api_key>
--- scrapper.py
+++ scrapper.py
@@ -5,34 +5,30 @@ from __future__ import unicode_literals, absolute_import
 import argparse
 from datetime import datetime
 import re
-from time import time, sleep
+import json
 
 from bs4 import BeautifulSoup
 from pushbullet import PushBullet
 import requests
 from requests.exceptions import RequestException
 
-import config
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-r', '--regex', dest='regex', help='Regex to match')
-parser.add_argument('-u', '--url', dest='url', help='URL to check')
-parser.add_argument('-m', '--mode', dest='mode', help='Mode for matching (FOUND or NOT_FOUND')
+parser.add_argument('-r', '--region', dest='region', help='Region (us, ca, uk, etc.)', default='us')
+parser.add_argument('-a', '--app', dest='game_id', help='Game ID')
+parser.add_argument('-k', '--key', dest='key', help='PushBullet API Key')
 
 args = parser.parse_args()
 
 HEADERS = {
     'User-Agent':
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Cache-Control': 'no-cache, no-store, must-revalidate',
     'Pragma': 'no-cache',
     'Expires': '0'
 }
-
-REGEX_FOUND_MESSAGE = "Regex found"
-REGEX_NOT_FOUND_MESSAGE = "Regex not found"
 
 
 def get_response_from_site(site_url):
@@ -51,57 +47,70 @@ def get_response_from_site(site_url):
     return site_response
 
 
-def send_notification(api_key, page_title, site_url):
+def send_notification(api_key, page_title, site_url, body=None):
 
     print "Sending PushBullet notification"
 
     pb_client = PushBullet(api_key)
 
-    pb_client.push_link("Update on {title}!".format(title=page_title), site_url)
+    pb_client.push_link("Price change for {title}!".format(title=page_title),
+                        site_url, body=body)
+
+
+def get_last_saved_prices():
+    with open('prices.json', mode='r') as save_file:
+        file_content = save_file.read()
+        if not file_content:
+            file_content = '{}'
+
+    return json.loads(file_content)
+
+
+def write_current_prices(prices):
+    with open('prices.json', mode="w") as save_file:
+        file_content = json.dumps(prices, indent=4)
+        save_file.write(file_content)
 
 
 def main():
 
-    config.check_config_file()
-    app_config = config.load_config_file()
-    pushbullet_api_key = app_config.get('api_key')
+    region = args.region
+    game_ids = args.game_id.split(',')
+    api_key = args.key
 
-    regex_to_test = getattr(args, 'regex', app_config.get('regex'))
-    if regex_to_test:
-        regex_to_test = re.compile(regex_to_test)
-    site_url = getattr(args, 'url', app_config.get('site_to_parse'))
-    if getattr(args, 'mode', False):
-        match_if_present = False if getattr(args, 'mode', None) == 'NOT_FOUND' else True
-    else:
-        match_if_present = False if app_config.get('match_mode') == 'NOT_FOUND' else True
-
-    if not pushbullet_api_key or not regex_to_test or not site_url:
+    if not api_key or not region or not game_ids:
         print "Some config values or arguments not given. Exiting..."
         exit(-1)
 
-    while True:
+    current_prices = get_last_saved_prices()
+
+    current_prices = {k: v for k, v in current_prices.items() if k in game_ids}
+
+    for gid in game_ids:
         print "\n", datetime.now()
+        site_url = "https://www.steamprices.com/{}/app/{}/".format(region, gid)
+
         response = get_response_from_site(site_url)
         site_content = BeautifulSoup(response.text)
-        page_title = site_content.find('title').text
-        matching_content = site_content.find_all(text=regex_to_test)
-        matching_content += site_content.find_all('a', href=regex_to_test)
+        page_title = site_content.find('h1', class_="title").a.text.strip()
+        text_price = site_content.find('td', class_="price_curent").span.text.strip()
 
-        if match_if_present:
-            if matching_content:
-                print REGEX_FOUND_MESSAGE
-                send_notification(pushbullet_api_key, page_title, site_url)
-                break
-            print REGEX_NOT_FOUND_MESSAGE
+        price_regex = re.compile('\d+(?:\.\d+)?')
 
+        parsed_price = price_regex.findall(text_price)
+        if parsed_price:
+            price = parsed_price[0]
         else:
-            if not matching_content:
-                print REGEX_NOT_FOUND_MESSAGE
-                send_notification(pushbullet_api_key, page_title, site_url)
-                break
-            print REGEX_FOUND_MESSAGE
+            price = ""
 
-        sleep(10)
+        if price != current_prices.get(gid):
+            send_notification(api_key, page_title, site_url, body=text_price)
+            print "New price found for {}, {}".format(page_title, text_price)
+            current_prices[gid] = price
+        else:
+            print "Price hasn't changed, still {}".format(text_price)
+
+    write_current_prices(current_prices)
 
     return
 
