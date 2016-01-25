b470fa631204d33f76cf7de2ab710f2d32ddf934
MarcDufresne <marc.andre.dufresne@gmail.com>
2016-01-24 23:02:38 -0500

    Added Keybin support for price storage

--- scrapper.py
+++ scrapper.py
@@ -18,7 +18,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-r', '--region', dest='region', help='Region (us, ca, uk, etc.)', default='us')
 parser.add_argument('-a', '--app', dest='game_id', help='Game ID')
 parser.add_argument('-k', '--key', dest='key', help='PushBullet API Key')
-
+parser.add_argument('--use-keybin', dest='use_keybin', help="Use Keybin as Data Store", action="store_true")
+parser.add_argument('--keybin-host', dest='keybin_host', help="Keybin endpoint (with http, e.g. http://keybin.com/")
+parser.add_argument('--user', dest='keybin_user', help="Keybin username")
+parser.add_argument('--passwd', dest='keybin_pass', help="Keybin password")
+parser.set_defaults(use_keybin=False)
 args = parser.parse_args()
 
 HEADERS = {
@@ -28,6 +32,12 @@ HEADERS = {
     'Cache-Control': 'no-cache, no-store, must-revalidate',
     'Pragma': 'no-cache',
     'Expires': '0'
+}
+
+KEYBIN_INFO = {
+    'host': '',
+    'user': '',
+    'pass': ''
 }
 
 
@@ -57,19 +67,74 @@ def send_notification(api_key, page_title, site_url, body=None):
                         site_url, body=body)
 
 
-def get_last_saved_prices():
-    with open('prices.json', mode='r') as save_file:
-        file_content = save_file.read()
-        if not file_content:
-            file_content = '{}'
+def get_keybin_token():
+    keybin_resp = requests.post(
+            '{}token'.format(KEYBIN_INFO['host']),
+            json={'username': KEYBIN_INFO['user'],
+                  'password': KEYBIN_INFO['pass'],
+                  'client_id': 'steam_prices'}
+    )
+    try:
+        keybin_resp.raise_for_status()
+        full_token = keybin_resp.json()
+        token = full_token.get('token', {}).get('token')
+    except requests.HTTPError as e:
+        print "Could not generate a token for Keybin, {}".format(e.message)
+        token = None
+    return token
 
-    return json.loads(file_content)
+
+def get_keybin_data():
+    token = get_keybin_token()
+    keybin_resp = requests.get(
+        "{}user/{}/store/steam_prices".format(
+            KEYBIN_INFO['host'], token
+        )
+    )
+    try:
+        keybin_resp.raise_for_status()
+        full_store = keybin_resp.json()
+        content = full_store.get('value')
+    except requests.HTTPError as e:
+        print "Could not read data from Keybin, {}".format(e.message)
+        content = None
+    return content
 
 
-def write_current_prices(prices):
-    with open('prices.json', mode="w") as save_file:
-        file_content = json.dumps(prices, indent=4)
-        save_file.write(file_content)
+def write_keybin_data(data):
+    token = get_keybin_token()
+    keybin_resp = requests.post(
+        '{}user/{}/store/steam_prices'.format(
+            KEYBIN_INFO['host'], token
+        ),
+        json={'value': data}
+    )
+    try:
+        keybin_resp.raise_for_status()
+    except requests.HTTPError as e:
+        print "Could not save data to Keybin, {}".format(e.message)
+
+
+def get_last_saved_prices(use_keybin=False):
+    if not use_keybin:
+        with open('prices.json', mode='r') as save_file:
+            content = save_file.read()
+    else:
+        content = get_keybin_data()
+
+    if not content:
+        content = '{}'
+
+    return json.loads(content)
+
+
+def write_current_prices(prices, use_keybin=False):
+    json_prices = json.dumps(prices, indent=4)
+    if not use_keybin:
+        with open('prices.json', mode="w") as save_file:
+            save_file.write(json_prices)
+    else:
+        write_keybin_data(json_prices)
 
 
 def main():
@@ -82,7 +147,17 @@ def main():
         print "Some config values or arguments not given. Exiting..."
         exit(-1)
 
-    current_prices = get_last_saved_prices()
+    use_keybin = args.use_keybin
+    if use_keybin and (not args.keybin_user or not args.keybin_pass or not args.keybin_host):
+        print "Keybin credentials must be set if using Keybin"
+        use_keybin = False
+
+    if use_keybin:
+        KEYBIN_INFO['host'] = args.keybin_host
+        KEYBIN_INFO['user'] = args.keybin_user
+        KEYBIN_INFO['pass'] = args.keybin_pass
+
+    current_prices = get_last_saved_prices(use_keybin=use_keybin)
 
     current_prices = {k: v for k, v in current_prices.items() if k in game_ids}
 
@@ -110,7 +185,7 @@ def main():
         else:
             print "Price hasn't changed, still {}".format(text_price)
 
-    write_current_prices(current_prices)
+    write_current_prices(current_prices, use_keybin=use_keybin)
 
     return
 
