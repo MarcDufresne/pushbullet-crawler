2e73b67779c4c7fb74190759c9625ca95f17ec66
MarcDufresne <marc.andre.dufresne@gmail.com>
2016-01-28 21:25:56 -0500

    Implemented keybin-client

--- scrapper.py
+++ scrapper.py
@@ -6,6 +6,7 @@ import argparse
 from datetime import datetime
 import re
 import json
+import keybin_client
 
 from bs4 import BeautifulSoup
 from pushbullet import PushBullet
@@ -14,30 +15,29 @@ from requests.exceptions import RequestException
 
 
 parser = argparse.ArgumentParser()
-
-parser.add_argument('-r', '--region', dest='region', help='Region (us, ca, uk, etc.)', default='us')
+parser.add_argument('-r', '--region', dest='region',
+                    help='Region (us, ca, uk, etc.)', default='us')
 parser.add_argument('-a', '--app', dest='game_id', help='Game ID')
 parser.add_argument('-k', '--key', dest='key', help='PushBullet API Key')
-parser.add_argument('--use-keybin', dest='use_keybin', help="Use Keybin as Data Store", action="store_true")
-parser.add_argument('--keybin-host', dest='keybin_host', help="Keybin endpoint (with http, e.g. http://keybin.com/")
+parser.add_argument('--use-keybin', dest='use_keybin',
+                    help="Use Keybin as Data Store", action="store_true")
+parser.add_argument('--keybin-host', dest='keybin_host',
+                    help="Keybin endpoint (with http, e.g. http://keybin.com/")
 parser.add_argument('--user', dest='keybin_user', help="Keybin username")
 parser.add_argument('--passwd', dest='keybin_pass', help="Keybin password")
 parser.set_defaults(use_keybin=False)
 args = parser.parse_args()
 
+
 HEADERS = {
     'User-Agent':
-        'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/41.0.2228.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,'
+              'application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Cache-Control': 'no-cache, no-store, must-revalidate',
     'Pragma': 'no-cache',
     'Expires': '0'
-}
-
-KEYBIN_INFO = {
-    'host': '',
-    'user': '',
-    'pass': ''
 }
 
 
@@ -67,62 +67,16 @@ def send_notification(api_key, page_title, site_url, body=None):
                         site_url, body=body)
 
 
-def get_keybin_token():
-    keybin_resp = requests.post(
-            '{}token'.format(KEYBIN_INFO['host']),
-            json={'username': KEYBIN_INFO['user'],
-                  'password': KEYBIN_INFO['pass'],
-                  'client_id': 'steam_prices'}
-    )
-    try:
-        keybin_resp.raise_for_status()
-        full_token = keybin_resp.json()
-        token = full_token.get('token', {}).get('token')
-    except requests.HTTPError as e:
-        print "Could not generate a token for Keybin, {}".format(e.message)
-        token = None
-    return token
-
-
-def get_keybin_data():
-    token = get_keybin_token()
-    keybin_resp = requests.get(
-        "{}user/{}/store/steam_prices".format(
-            KEYBIN_INFO['host'], token
-        )
-    )
-    try:
-        keybin_resp.raise_for_status()
-        full_store = keybin_resp.json()
-        content = full_store.get('value')
-        print "Got value from Keybin"
-    except requests.HTTPError as e:
-        print "Could not read data from Keybin, {}".format(e.message)
-        content = None
-    return content
-
-
-def write_keybin_data(data):
-    token = get_keybin_token()
-    keybin_resp = requests.post(
-        '{}user/{}/store/steam_prices'.format(
-            KEYBIN_INFO['host'], token
-        ),
-        json={'value': data}
-    )
-    try:
-        keybin_resp.raise_for_status()
-        print "Wrote to Keybin"
-    except requests.HTTPError as e:
-        print "Could not save data to Keybin, {}".format(e.message)
-
-
 def get_last_saved_prices(use_keybin=False):
     if not use_keybin:
         with open('prices.json', mode='r') as save_file:
             content = save_file.read()
     else:
-        content = get_keybin_data()
+        try:
+            content = keybin_client.get_key_value('steam_prices')
+        except keybin_client.KeybinError as e:
+            print e.message
+            content = None
 
     if not content:
         content = '{}'
@@ -136,7 +90,10 @@ def write_current_prices(prices, use_keybin=False):
         with open('prices.json', mode="w") as save_file:
             save_file.write(json_prices)
     else:
-        write_keybin_data(json_prices)
+        try:
+            keybin_client.store_key_value('steam_prices', json_prices)
+        except keybin_client.KeybinError as e:
+            print e.message
 
 
 def main():
@@ -157,9 +114,8 @@ def main():
         use_keybin = False
 
     if use_keybin:
-        KEYBIN_INFO['host'] = args.keybin_host
-        KEYBIN_INFO['user'] = args.keybin_user
-        KEYBIN_INFO['pass'] = args.keybin_pass
+        keybin_client.init(args.keybin_host, args.keybin_user,
+                           args.keybin_pass, client_id='steam_price')
 
     current_prices = get_last_saved_prices(use_keybin=use_keybin)
 
